7d5a3f439fc725ae415ca72635442fb1467c2a6e
Kerem Berkkanoğlu <berkkanoglukerem@gmail.com>
2026-06-18 20:45:32 +0300

    need further research will be back soon

--- src/main/java/com/Backend/Spring/Security/JWT/JwtService.java
+++ src/main/java/com/Backend/Spring/Security/JWT/JwtService.java
@@ -1,6 +1,54 @@
 package com.Backend.Spring.Security.JWT;
-public class JwtService {
-    public static void main(String[] args) {
-        
+
+
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.io.Decoders;
+import io.jsonwebtoken.security.Keys;
+import lombok.Value;
+import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
+
+@Service
+public class JwtService{
+
+    @Value("${jwt.secret}")
+    private String secretKey;
+
+    @Value("${jwt.expiration}")
+    private long jwtExpiration;
+
+    private Key getSignInKey() {
+        byte[] keyBytes= Decoders.BASE64.decode(secretKey);
+        return Keys.hmacShaKeyFor(keyBytes);
     }
+
+    private Claims extractAllClaims(String token){
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignInKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    public <T>T extractClaim(String token, Function<Claims,T> claimsResolver){
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
+
+    private Date extractExpiration(String token){
+        return extractClaim(token , Claims::getExpiration );
+    }
+
+    private boolean isTokenExpired(String token){
+        return extractExpiration(token).before(new Date());
+    }
+
+
+
+
+
 }
